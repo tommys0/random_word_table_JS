8f3000f63f5e79d8b6b5acdfce6c83ad84a11631
tommys0 <149568540+tommys0@users.noreply.github.com>
2023-12-05 17:36:15 +0100

    Added generateTable function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const words = ['Jablko', 'Houska', 'Auto', 'Stůl', 'Kočka', 'Pes', 'Kytara', 'Kniha', 'Mobil', 'Hodinky'];
+const words = ['Auto', 'Kamion', 'Trajekt', 'Letadlo', 'Helikoptera', 'Motorka', 'Kolo', 'Skutr', 'Ctyrkolka', 'Kroska', 'Lod', 'Stihacka', 'Trikolka'];
 
 function generateTable() {
+    const body = document.querySelector('body');
 
+    const table = document.createElement('table');
+    table.setAttribute('id', 'myTable');
+    body.appendChild(table);
+
+    const headers = ['Num.1', 'Num.2', 'Num.3'];
+
+    const headerRow = table.createTHead().insertRow();
+    headers.forEach(headerText => {
+        const th = document.createElement('th');
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+
+    for (let i = 0; i < 10; i++) {
+        const row = table.insertRow();
+        for (let j = 0; j < 3; j++) {
+            const cell = row.insertCell();
+            const randomWord = words[Math.floor(Math.random() * words.length)];
+            cell.textContent = randomWord;
+        }
+    }
 }
 
 function addRow() {
@@ -15,3 +37,5 @@ function deleteRow() {
 function randomRows() {
 
 }
+
+generateTable()
