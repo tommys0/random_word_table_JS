59d3a6b13a4e4759a17d162275865455d38b4e33
tommys0 <149568540+tommys0@users.noreply.github.com>
2023-12-05 16:58:15 +0100

    Structure Prepare

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log('Test')
+const words = ['Jablko', 'Houska', 'Auto', 'Stůl', 'Kočka', 'Pes', 'Kytara', 'Kniha', 'Mobil', 'Hodinky'];
 
+function generateTable() {
+
+}
+
+function addRow() {
+
+}
+
+function deleteRow() {
+
+}
+
+function randomRows() {
+
+}
